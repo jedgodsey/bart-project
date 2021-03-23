33b069183b120717b271ed49cd3327cb2669bc53
jedgodsey <jed.godsey@gmail.com>
2021-03-23 15:13:06 -0700

    front connected to api

--- bart/tasks.py
+++ bart/tasks.py
@@ -8,8 +8,8 @@ channel_layer = get_channel_layer()
 
 @shared_task
 def get_train():
-    url = 'http://api.icndb.com/jokes/random/'
+    url = 'https://api.bart.gov/api/etd.aspx?cmd=etd&orig=all&key=MW9S-E7SL-26DU-VV8V&json=y'
     response = requests.get(url).json()
-    train = response['value']['joke']
+    train = response['root']['station'][0]['name']
     
     async_to_sync(channel_layer.group_send)('trains', {'type': 'send_trains', 'text': train})
