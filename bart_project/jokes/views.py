f2d16d2fa5f59419c0a2e5422483745ca92c602f
jedgodsey <jed.godsey@gmail.com>
2021-03-22 20:02:13 -0700

    first commit

---
+++ bart_project/jokes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import requests
+# import json
+
+def index(request):
+    url = 'http://api.icndb.com/jokes/random/'
+    response = requests.get(url).json()
+    joke = response['value']['joke']
+    return render(request, 'index.html', context={'text': joke})
